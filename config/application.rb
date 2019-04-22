561467d41110d2b3547d6631e54fdd740a1899d0
PaulWork <islandertron@gmail.com>
2019-04-22 15:23:02 -0400

    Testing case for layout and links

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
+# Require the s listed in file, including any s
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
@@ -14,6 +14,6 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # the framework and any s in your application.
   end
 end
